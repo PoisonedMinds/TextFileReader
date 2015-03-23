f2e0a6dd1f12bfd3a7b5aac6013904d6725398b4
PoisonedMinds <steven_biro@hotmail.com>
2015-03-23 09:26:48 -0400

    commenting

--- TextFileReader/src/textfilereader/TextFileReader.java
+++ TextFileReader/src/textfilereader/TextFileReader.java
@@ -1,42 +1,47 @@
-
-
 package textfilereader;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.*;
+
 /**
  * @title TextFileReader
  * @author Steven Biro
  * @teacher Mr. J. Carron
  * @date 10-Mar-2015 10:14:48 AM
- * @purpose The purpose of this program is to
+ * @purpose The purpose of this program is to read text files
  */
 public class TextFileReader {
 
     public static void main(String[] args) {
-        String path,line;
-        int test = 0;
+        String path, line;
+        int test;
 
         do {
-        path=JOptionPane.showInputDialog("Input the path of the file you want to read. \nEx. 'c:\\myfile.txt'");
-        try {
-            BufferedReader br = new BufferedReader(
-                    new FileReader(path));
+            test = 0;
+            //get path to file
+            path = JOptionPane.showInputDialog("Input the path of the file you want to read. \nEx. 'c:\\myfile.txt'");
+            //start buffered reader, catch errors 
+            try {
+                BufferedReader br = new BufferedReader(
+                        new FileReader(path));
 
-             while( (line=br.readLine()) != null) {
-                    System.out.println(line);  
-             }
-        } catch (FileNotFoundException ex) {
-            test=1;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (FileNotFoundException ex) {
+                test = 1;
+                //warn that the path given is invalid
+                System.out.println("file not found.");
 
-        }   catch (IOException ex) {
-            System.out.println("An error has occured, try again");
-            test=1;
-        }} while(test==1);
+            } catch (IOException ex) {
+                System.out.println("An error has occured, try again");
+                test = 1;
+            }
+            //retry if an error occured
+        } while (test == 1);
 
     }
 
